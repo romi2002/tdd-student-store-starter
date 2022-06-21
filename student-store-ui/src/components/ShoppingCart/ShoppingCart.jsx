19d64e5e67ad66177bfd98182909fc5f9e565a7e
Abiel Fernandez <abielfdz@fb.com>
2022-06-20 17:30:28 -0700

    Refactored shopping cart

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,26 +2,28 @@ import * as React from "react"
 import "./ShoppingCart.css"
 import {ShoppingCart as ShoppingCartIcon} from "@material-ui/icons";
 
+//TODO Check classnames for autograder
 export default function ShoppingCart({products, shoppingCart}){
-    const hasItems = shoppingCart.length && shoppingCart.reduce((partialSum, n) => partialSum + n) === 0;
-    //const totalPrice = shoppingCart.reduce((partialSum, item))
+    const totalPrice = shoppingCart.length ? shoppingCart.slice(1).reduce((total, quantity, id) => {
+        if(quantity && quantity !== 0) return total + quantity * products[id].price
+        else return total
+    }) : 0;
+
+    const hasItems = totalPrice !== 0; //Maybe not a good idea in case there's a free item
+
+    console.log(`totalPrice: ${totalPrice} hasItems: ${hasItems}`)
 
     return (
         <div className='shopping-cart'>
             <ShoppingCartIcon/>
-            {hasItems && (
+            {!hasItems && (
                 <span className='notification'>No items added to cart yet. Start shopping now!</span>
                 )}
             {shoppingCart && shoppingCart.map((item, index) => {
-
-                const product = products.find(p => p.id === index)
-
-                console.log(`index: ${index} product: ${product}`)
-                if(product && item){
-                    return (<div key={'item-'+index}>
-                        <h6>{product.name + item}</h6>
-                    </div>)
-                }
+                const product = products.find((p) => p.id === index)
+                //TODO Format items
+                return (item &&
+                        <h6>{item}</h6>)
             })}
         </div>
     )
